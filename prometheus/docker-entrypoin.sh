cd972bda8d3716fd85c32ca64c37a29dcedf71b2
addicola <blue_lifesky@hotmail.com>
2025-04-21 01:49:31 +0700

    genarate prometheus config from script

---
+++ prometheus/docker-entrypoin.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+cat <<EOF >> /etc/prometheus/prometheus.yaml
+global:
+  scrape_interval: 15s
+  evaluation_interval: 15s
+
+alerting:
+  alertmanagers:
+    - timeout: 10s
+    - static_configs:
+        - targets: ['${ALERTMANAGER_TARGET:-alertmanager:9093}']
+
+rule_files:
+  - /etc/prometheus/rules/*.yaml
+
+scrape_configs:
+  - job_name: aptos
+    static_configs:
+      - targets:
+          [
+            # Validator
+            "${TESTNET_VALIDATOR_IP}:${TESTNET_VALIDATOR_PORT}",
+
+            # VFN
+            "${TESTNET_VFN_IP}:${TESTNET_VFN_PORT}",
+          ]
+        labels:
+          chain: testnet
+EOF
+
+# Start Prometheus with the generated configuration
+exec prometheus --config.file=/etc/prometheus/${PROM_CONFIG_FILE:-prometheus.yaml} "$@"
